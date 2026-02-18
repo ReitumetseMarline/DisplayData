605c697b34b5a6b7763e48228608b83ee84c09dc
Reitumetse Marline Mosehla <remosehjhb024@student.wethinkcode.co.za>
2026-02-18 17:42:52 +0200

    added the logic for displaying data

--- display.py
+++ display.py
@@ -1,6 +1,30 @@
 import json
+import matplotlib.pyplot as plt
 
 with open("covid.json", "r") as file:
     data = json.load(file)
 
-    print(data)
+dates = []
+daily_cases = []
+daily_deaths = []
+
+for entry in data:
+    dates.append(entry["Date"])
+
+    cases = entry["Daily Confirmed Cases"]
+    deaths = entry["Daily  deaths"]
+
+    daily_cases.append(cases)
+    daily_deaths.append(deaths)
+
+plt.plot(dates, daily_cases)
+plt.plot(dates, daily_deaths)
+
+plt.xlabel("Date")
+plt.ylabel("Count")
+plt.title("Daily COVID Cases and Deaths")
+plt.xticks(rotation=45)
+plt.legend(["Daily Cases", "Daily Deaths"])
+
+plt.tight_layout()
+plt.show()
