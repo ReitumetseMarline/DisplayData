a84c515fc0a1d67bd7e8de37fcd5a72eecb12ecf
Reitumetse Marline Mosehla <remosehjhb024@student.wethinkcode.co.za>
2026-02-18 17:45:14 +0200

    Converted dates of strings to date

--- display.py
+++ display.py
@@ -1,5 +1,7 @@
 import json
 import matplotlib.pyplot as plt
+from datetime import datetime
+import matplotlib.dates as mdates
 
 with open("covid.json", "r") as file:
     data = json.load(file)
@@ -9,7 +11,7 @@ daily_cases = []
 daily_deaths = []
 
 for entry in data:
-    dates.append(entry["Date"])
+    dates.append(datetime.strptime(entry["Date"], "%Y/%m/%d"))
 
     cases = entry["Daily Confirmed Cases"]
     deaths = entry["Daily  deaths"]
@@ -17,12 +19,17 @@ for entry in data:
     daily_cases.append(cases)
     daily_deaths.append(deaths)
 
+plt.figure(figsize=(10, 5))
 plt.plot(dates, daily_cases)
 plt.plot(dates, daily_deaths)
 
 plt.xlabel("Date")
 plt.ylabel("Count")
 plt.title("Daily COVID Cases and Deaths")
+
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=5))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+
 plt.xticks(rotation=45)
 plt.legend(["Daily Cases", "Daily Deaths"])
 
